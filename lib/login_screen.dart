a1248367ded0cdc2be5d6950e4ec4f43260ec938
birugh <notblue.https@gmail.com>
2025-04-27 13:35:48 +0700

    init

---
+++ lib/login_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lks_project_new/register_screen.dart';
+
+class LoginScreenStateful extends StatefulWidget {
+  const LoginScreenStateful({super.key});
+
+  @override
+  State<LoginScreenStateful> createState() => _LoginScreenStatefulState();
+}
+
+class _LoginScreenStatefulState extends State<LoginScreenStateful> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  HeaderApp(context),
+                  SizedBox(height: 20),
+                  TitleApp(context),
+                  SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 40),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: _usernameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please fil the username';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 40),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please fill the Password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  SizedBox(
+                    width: 330,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // asad
+                      },
+                      child: Text('Login'),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RegisterScreenStateful(),
+                        ),
+                      );
+                    },
+                    child: Text('Belum punya akun? Daftar disini!'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget HeaderApp(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/Logo.png', height: 100),
+          Text(
+            'LKS\nMart',
+            style: TextStyle(
+              fontSize: 42,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xFF486b8b),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget TitleApp(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Sign In',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Enter your username and password to sign in!',
+              style: TextStyle(fontSize: 12, color: Color(0xFFb5b5b5)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreenStateful extends StatefulWidget {
+  const RegisterScreenStateful({super.key});
+
+  @override
+  State<RegisterScreenStateful> createState() => _RegisterScreenStatefulState();
+}
+
+class _RegisterScreenStatefulState extends State<RegisterScreenStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              HeaderApp(context),
+              SizedBox(height: 10),
+              TitlePage(context),
+              SizedBox(height: 20),
+              FormPage(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget HeaderApp(BuildContext context) {
+  return Container(
+    child: Column(
+      children: [
+        Image.asset('assets/Logo.png', height: 100),
+        Text(
+          'LKS MART',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF3c6283),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget TitlePage(BuildContext context) {
+  return SizedBox(
+    width: double.infinity,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 40, right: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Daftar',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Silahkan isi Data Pribadi anda',
+            style: TextStyle(fontSize: 18, color: Color(0xFFb2b2b2)),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget FormPage(BuildContext context) {
+  final namaLengkapController = TextEditingController();
+  final alamatController = TextEditingController();
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPassController = TextEditingController();
+  return Container(
+    child: Padding(
+      padding: const EdgeInsets.only(left: 40, right: 40),
+      child: Form(
+        child: Center(
+          child: Column(
+            children: [
+              TextField(
+                context,
+                'Nama Lengkap',
+                'Nama Lengkap',
+                namaLengkapController,
+              ),
+              TextField(context, 'Alamat', 'Alamat', alamatController),
+              TextField(context, 'Username', 'Username', usernameController),
+              TextField(context, 'Password', 'Password', passwordController),
+              TextField(
+                context,
+                'Konfirmasi Password',
+                'Konfirmasi Password',
+                confirmPassController,
+              ),
+              SizedBox(
+                width: 500,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //
+                  },
+                  child: Text('Daftar'),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  //
+                },
+                child: Text('Sudah punya akun? Daftar di sini!'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget TextField(
+  BuildContext context,
+  String txtTitle,
+  String txtLabel,
+  var controller,
+) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        txtTitle,
+        style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+      ),
+      SizedBox(height: 10),
+      SizedBox(
+        height: 50,
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: txtLabel,
+            labelStyle: TextStyle(fontSize: 14),
+          ),
+          controller: controller,
+        ),
+      ),
+      SizedBox(height: 10),
+    ],
+  );
+}
